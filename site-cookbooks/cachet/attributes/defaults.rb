6210238a1095214b836a788c46757d8f9a62f7a7
sawanoboly <sawanoboriyu@higanworks.com>
2016-02-14 11:53:35 +0900

    2.1.1

--- site-cookbooks/cachet/attributes/defaults.rb
+++ site-cookbooks/cachet/attributes/defaults.rb
@@ -2,6 +2,6 @@ default[:lw1_cachet][:user] = 'apache'
 default[:lw1_cachet][:group] = 'apache'
 
 default[:lw1_cachet][:install] = {
-  version: 'v2.1.0',
+  version: 'v2.1.1',
   repo_url: "https://github.com/CachetHQ/Cachet.git"
 }
